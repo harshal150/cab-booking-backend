d55ae10ffceb9ac7ee282f53f02551a1f6176b11
Harshal <harshhh30601@gmail.com>
2025-01-13 17:57:11 +0530

    port change to 5001

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ db.getConnection()
     });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
